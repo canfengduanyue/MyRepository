46096ef2b1c50fb4b7c4a4f5562460696c870743
canfengduanyue <31273916+canfengduanyue@users.noreply.github.com>
2017-08-28 19:06:54 +0800

    0828study

---
+++ MyJavaProject/src/com/cguo/basicDataType/IntegerDemo.java
@@ -0,0 +1,23 @@
+package com.cguo.basicDataType;
+
+/**
+ * @author GuoCheng
+ * 基本数据类型包装类：
+ * 	byte Byte
+ *  short Short
+ *  int Integer
+ *  long Long
+ *  boolean Boolean
+ *  float Float
+ *  double Double
+ *  char Character
+ *  
+ *  作用：
+ *  	用于基本数据类型和字符串之间做转换
+ *  
+ */
+public class IntegerDemo {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ MyJavaProject/src/com/cguo/string/TestStringBuffer.java
@@ -0,0 +1,30 @@
+package com.cguo.string;
+
+/**
+ * @author GuoCheng
+ * StringBuffer
+ * 线程同步的
+ */
+public class TestStringBuffer {
+	public static void main(String[] args) {
+		
+		TestStringBuffer.add();
+		TestStringBuffer.delete();
+	}
+	
+	//存储
+	public static void add() {
+		StringBuffer sb = new StringBuffer();
+		sb.append("abc").append(123).append(true);
+		System.out.println(sb.toString());
+		sb.insert(1, "mm");
+		System.out.println(sb.toString());
+	}
+	
+	//删除
+	public static void delete() {
+		StringBuffer sb = new StringBuffer("abcde");
+		sb.delete(1, 3);
+		System.out.println(sb.toString());
+	}
+}
